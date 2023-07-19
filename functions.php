8eb72a955da7a31071d51761ffb6036ef48ab240
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-07-19 15:24:10 +0200

    first commit

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+  // require_once get_template_directory() . '/includes/styles-scripts.php';
+  // require_once get_template_directory() . '/includes/acf-functions.php';
+  // require_once get_template_directory() . '/includes/acf-blocks.php';
+  // require_once get_template_directory() . '/includes/menus.php';
+  // require_once get_template_directory() . '/includes/helpers.php';
+  // require_once get_template_directory() . '/includes/registers.php';
+  // require_once get_template_directory() . '/includes/filters.php';
+  // require_once get_template_directory() . '/includes/shortcodes.php';

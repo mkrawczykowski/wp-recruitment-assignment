d54600c8d200d41d214d430d31c8e5d9e67ac137
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-07-20 13:43:54 +0200

    RM

--- functions.php
+++ functions.php
@@ -2,7 +2,6 @@
 
 <?php
   require_once get_template_directory() . '/includes/styles-scripts.php';
-  // require_once get_template_directory() . '/includes/acf-functions.php';
   require_once get_template_directory() . '/includes/blocks.php';
   // require_once get_template_directory() . '/includes/menus.php';
   // require_once get_template_directory() . '/includes/helpers.php';

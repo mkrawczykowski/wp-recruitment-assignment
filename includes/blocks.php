3fbf813d13d7680e0033b687d75e3cf2add6bcc9
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-07-20 14:50:25 +0200

    remove styles

--- includes/blocks.php
+++ includes/blocks.php
@@ -1,7 +1,21 @@
 <?php defined('ABSPATH') or die; ?>
 
 <?php
-// Register ACF blocks via block.json 
+
+/**
+ * Remove Gutenberg Block Library CSS from loading on the frontend
+ */
+function remove_wp_block_library_css(){
+  wp_dequeue_style( 'wp-block-library' );
+  wp_dequeue_style( 'wp-block-library-theme' );
+  wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+} 
+add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css', 100 );
+
+
+/** 
+ * Register ACF blocks via block.json 
+ */
 function register_acf_blocks() {
     register_block_type( get_template_directory() . '/build/blocks/block-1');
 }

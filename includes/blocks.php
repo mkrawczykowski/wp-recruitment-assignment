997dd0ee6d9aca94271fd9541f13770bc7c924de
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-07-21 10:00:00 +0200

    footer css & block hero-slider

--- includes/blocks.php
+++ includes/blocks.php
@@ -18,6 +18,7 @@ add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css', 100 );
  */
 function register_acf_blocks() {
     register_block_type( get_template_directory() . '/build/blocks/block-1');
+    register_block_type( get_template_directory() . '/build/blocks/hero-slider');
 }
 add_action( 'init', 'register_acf_blocks', 5 );
 ?>
